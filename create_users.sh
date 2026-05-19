1c0d32271d042b54ab8c22447d39d2c57fb6418e
Micaela Egger <micaela.egger@svt.se>
2026-05-19 11:23:17 +0200

    Add create_users.sh

---
+++ create_users.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script som skapar nya användare med hemkatalog, mappar och välkomstfil
+# Används av systemadministratörer för att onboarda nya användare
+
+# Kontrollera att scriptet körs som root (administratör)
+if [ "$EUID" -ne 0 ]; then
+  echo "Fel: Scriptet måste köras som root. Använd sudo."
+  exit 1
+fi
+
+# Loopa igenom alla användarnamn som skickas in som argument
+for username in "$@"; do
+
+  # Skapa användarkontot med en hemkatalog
+  useradd -m "$username"
+
+  HOME_DIR="/home/$username"
+
+  # Skapa standardmapparna i hemkatalogen
+  mkdir -p "$HOME_DIR/Documents" "$HOME_DIR/Downloads" "$HOME_DIR/Work"
+
+  # Sätt ägare och begränsa rättigheter (700 = bara ägaren kan läsa/skriva)
+  chown -R "$username":"$username" "$HOME_DIR"
+  chmod 700 "$HOME_DIR/Documents" "$HOME_DIR/Downloads" "$HOME_DIR/Work"
+
+  # Skapa välkomstfilen med en hälsning och lista på övriga användare
+  echo "Välkommen $username" > "$HOME_DIR/welcome.txt"
+  echo "Övriga användare på systemet:" >> "$HOME_DIR/welcome.txt"
+  cut -d: -f1 /etc/passwd | grep -v "^$username$" >> "$HOME_DIR/welcome.txt"
+
+  # Sätt rätt ägare på välkomstfilen
+  chown "$username":"$username" "$HOME_DIR/welcome.txt"
+
+done
